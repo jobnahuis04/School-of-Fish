d31216c56091080d6ea487a2e02ce99b1c48c60a
Job Nahuis <jobnahuis@gmail.com>
2026-08-19 23:24:25 +0200

    first progress

--- Fish.cpp
+++ Fish.cpp
@@ -10,18 +10,21 @@
 using namespace std;
 
 void School::schoolDynamics() {
-    int N = number_of_fish;
-    // initialize an N x N matrix pre-filled with 0.0f
-
-    std::vector<std::vector<float>> fish_separation_error(N, std::vector<float>(N, 0.0f));    for (int i = 0; i < number_of_fish; i++) {
+    for (int i = 0; i < number_of_fish; i++) {
         for (int j = i+1; j < number_of_fish; j++) { // looping over all fish without going over fish twice
             array<float,3> pos_i = getFishById(i)->position;
             array<float,3> pos_j = getFishById(j)->position;
-            float error = distanceBetweenFish(pos_i, pos_j)-ideal_separation;
-            fish_separation_error[i][j] = error;
-            fish_separation_error[j][i] = error;
+            DistanceAndDirectionBetweenFish d_fish = distanceBetweenFish(pos_i, pos_j);
+            float fish_separation_error = d_fish.distance-ideal_separation;
+            float F = forceBetweenFish(fish_separation_error);
+            getFishById(i)->fish_force = {F*d_fish.direction[0],F*d_fish.direction[1],F*d_fish.direction[2]};
+            getFishById(j)->fish_force = {-F*d_fish.direction[0],-F*d_fish.direction[1],-F*d_fish.direction[2]};
         }
     }
+}
+
+float School::forceBetweenFish(float err) {
+    return fish_force_factor / (err * err);
 }
 
 // Returns a pointer to the Fish (or nullptr if not found)
@@ -33,12 +36,17 @@ Fish* School::getFishById(int target_id) {
     return (it != fish.end()) ? &(*it) : nullptr;
 }
 
-float School::distanceBetweenFish(const array<float,3> pos1, const array<float,3> pos2) {
+
+School::DistanceAndDirectionBetweenFish School::distanceBetweenFish(const array<float,3> pos1, const array<float,3> pos2) {
     //TODO: check if it is possible to keep a squared distance to avoid taking the square root
-    const float dx = pos1[0] - pos2[0];
-    const float dy = pos1[1] - pos2[1];
-    const float dz = pos1[2] - pos2[2];
+    const float dx = pos2[0] - pos1[0];
+    const float dy = pos2[1] - pos1[1];
+    const float dz = pos2[2] - pos1[2];
     const float d_squared = dx * dx + dy * dy + dz * dz;
-    return sqrt(d_squared);
+    const float dist = sqrt(d_squared);
+    DistanceAndDirectionBetweenFish result;
+    result.distance = sqrt(d_squared);
+    result.direction = {dx/dist, dy/dist, dz/dist}; // normalized direction from pos1 to pos2
+    return result;
 }
 
--- Fish.h
+++ Fish.h
@@ -11,8 +11,9 @@ using namespace std;
 class Fish {
 public:
     int id; // unique id of the fish in the school
-    array<float,3> position; // position of the fish (x, y, z)
-    array<float,3> velocity; // velocity of the fish (x, y, z)
+    array<float,3> position = {0.0f, 0.0f, 0.0f}; // position of the fish (x, y, z)
+    array<float,3> velocity = {0.0f, 0.0f, 0.0f}; // velocity of the fish (x, y, z)
+    array<float,3> fish_force = {0.0f, 0.0f, 0.0f}; // force on the fish (x, y, z)
 };
 
 class School {
@@ -23,13 +24,21 @@ public:
     float ideal_separation; // ideal distance between each fish
     float fear_factor; // how afraid the fish are of supposed predators
     float nearby_fish_radius; // the radius around a fish of other fish that it wants to swim next to
+    float fish_force_factor; // scaling factor of the force between fish
     float random_velocity_scaling; // each fish can randomly change heading;
                                    // this factor determines how large the randomization effect is
 
     vector<Fish> fish;
     void schoolDynamics();
     Fish* getFishById(int target_id);
-    static float distanceBetweenFish(array<float,3> pos1, array<float,3> pos2);
+    struct DistanceAndDirectionBetweenFish {
+        float distance;
+        array<float,3> direction;
+    };
+    DistanceAndDirectionBetweenFish distanceBetweenFish(array<float,3> pos1, array<float,3> pos2);
+    float forceBetweenFish(float err, );
+
+
 };
 
 
